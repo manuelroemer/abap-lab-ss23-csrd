0a4ba594d095334cf6ccc84cbde25f125f22cb19
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2023-05-17 19:13:07 +0200

    Test

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,3 +1,5 @@
 import Controller from 'sap/ui/core/mvc/Controller';
 
-export default class AppController extends Controller {}
+export default class AppController extends Controller {
+  foo: number = '123';
+}
