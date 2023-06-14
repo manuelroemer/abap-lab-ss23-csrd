a0a52926c2140ff043e23d05bc9a48650013fb57
LuckyG0ldfish <tim-koeneke@online.de>
2023-06-14 17:10:51 +0200

    energy consumption pages

--- webapp/formengine/DemoFormSchema.ts
+++ webapp/formengine/DemoFormSchema.ts
@@ -3,6 +3,217 @@ import { FormSchema } from './Schema';
 export const demoFormSchema: FormSchema = {
   pages: [
     {
+      id: 'consumption-check',
+      title: 'Consumption-Check',
+      elements: [
+        {
+          type: 'heading',
+          text: 'Energy consumption',
+        },
+        {
+          type: 'text',
+          text: 'Which sources of energy does your company use?',
+        },
+        {
+          type: 'multi-choice',
+          id: 'Multi-Choice1',
+          options: [
+            { value: 'coal', display: 'coal and coal products' },
+            { value: 'petroleum', display: 'crude oil and petroleum products' },
+            { value: 'gas', display: 'natural gas' },
+            { value: 'other-non-renewable', display: 'other non-renewable sources' },
+            { value: 'nuclear', display: 'nuclear products' },
+            { value: 'purchased-non-renew', display: 'purchased or acquired electricity, heat, steam, and cooling from non-renewable sources' },
+          ],
+          label: 'Non-renewable Sources',
+          required: false,
+        },
+        {
+          type: 'multi-choice',
+          id: 'Multi-Choice2',
+          options: [
+            { value: 'renew-fuel', display: 'renewable fuel sources' },
+            { value: 'purchased-renew', display: 'consumption of purchased or acquired electricity, heat, steam, and cooling from renewable sources' },
+            { value: 'self-generated', display: 'self-generated non-fuel renewable energy' },
+          ],
+          label: 'Renewable Sources',
+          required: false,
+        },
+      ],
+    },
+    {
+      id: 'consumption-values',
+      title: 'Consumption-Values',
+      elements: [
+        {
+          type: 'heading',
+          text: 'Energy consumption',
+        },
+        {
+          type: 'text-input', 
+          id: 'TotalEnergyConsumption',
+          placeholder: 'Total energy consumption in MWH',
+          label: 'Total energy consumption in MWH:',
+          description: 'What is the total energy consumption of your company?',
+          required: false,
+        },
+        {
+          type: 'text-input', 
+          id: 'coal-amount',
+          placeholder: 'energy consumption in MWH',
+          label: 'Energy consumption from coal and coal products',
+          required: false,
+          effects: [
+            {
+              effect: 'hide',
+              condition: {
+                type: 'eq',
+                left: { type: 'value', id: 'Multi-Choice1.coal' },
+                right: true,
+              },
+            },
+          ],
+        }, 
+        {
+          type: 'text-input', 
+          id: 'petroleum-amount',
+          placeholder: 'energy consumption in MWH',
+          label: 'Energy consumption from crude oil and petroleum products',
+          required: false,
+          effects: [
+            {
+              effect: 'hide',
+              condition: {
+                type: 'eq',
+                left: { type: 'value', id: 'Multi-Choice1.petroleum' },
+                right: true,
+              },
+            },
+          ],
+        },
+        {
+          type: 'text-input', 
+          id: 'gas-amount',
+          placeholder: 'energy consumption in MWH',
+          label: 'Energy consumption from natural gas',
+          required: false,
+          effects: [
+            {
+              effect: 'hide',
+              condition: {
+                type: 'eq',
+                left: { type: 'value', id: 'Multi-Choice1.gas' },
+                right: true,
+              },
+            },
+          ],
+        }, 
+        {
+          type: 'text-input', 
+          id: 'other-non-renewable-amount',
+          placeholder: 'energy consumption in MWH',
+          label: 'Energy consumption from other non-renewable sources',
+          required: false,
+          effects: [
+            {
+              effect: 'hide',
+              condition: {
+                type: 'eq',
+                left: { type: 'value', id: 'Multi-Choice1.other-non-renewable' },
+                right: true,
+              },
+            },
+          ],
+        }, 
+        {
+          type: 'text-input', 
+          id: 'nuclear-amount',
+          placeholder: 'energy consumption in MWH',
+          label: 'Energy consumption from nuclear products',
+          required: false,
+          effects: [
+            {
+              effect: 'hide',
+              condition: {
+                type: 'eq',
+                left: { type: 'value', id: 'Multi-Choice1.nuclear' },
+                right: true,
+              },
+            },
+          ],
+        }, 
+        {
+          type: 'text-input', 
+          id: 'purchased-non-renew-amount',
+          placeholder: 'energy consumption in MWH',
+          label: 'Energy consumption from purchased or acquired electricity, heat, steam, and cooling from non-renewable sources',
+          required: false,
+          effects: [
+            {
+              effect: 'hide',
+              condition: {
+                type: 'eq',
+                left: { type: 'value', id: 'Multi-Choice1.purchased-non-renew' },
+                right: true,
+              },
+            },
+          ],
+        },
+        {
+          type: 'text-input', 
+          id: 'renew-fuel-amount',
+          placeholder: 'energy consumption in MWH',
+          label: 'Energy consumption from renewable fuel sources',
+          required: false,
+          effects: [
+            {
+              effect: 'hide',
+              condition: {
+                type: 'eq',
+                left: { type: 'value', id: 'Multi-Choice2.renew-fuel' },
+                right: true,
+              },
+            },
+          ],
+        }, 
+        {
+          type: 'text-input', 
+          id: 'purchased-renew-amount',
+          placeholder: 'energy consumption in MWH',
+          label: 'Energy consumption of purchased or acquired electricity, heat, steam, and cooling from renewable sources',
+          required: false,
+          effects: [
+            {
+              effect: 'hide',
+              condition: {
+                type: 'eq',
+                left: { type: 'value', id: 'Multi-Choice2.purchased-renew' },
+                right: true,
+              },
+            },
+          ],
+        }, 
+        {
+          type: 'text-input', 
+          id: 'self-generated-amount',
+          placeholder: 'energy consumption in MWH',
+          label: 'Energy consumption from self-generated non-fuel renewable energy',
+          required: false,
+          effects: [
+            {
+              effect: 'hide',
+              condition: {
+                type: 'eq',
+                left: { type: 'value', id: 'Multi-Choice2.self-generated' },
+                right: true,
+              },
+            },
+          ],
+        }, 
+      ],
+    },
+
+    {
       id: 'page1',
       title: 'Page 1',
       elements: [
