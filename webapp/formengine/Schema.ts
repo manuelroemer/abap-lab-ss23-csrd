17d751da74516322e3ef196cf4abf216f0b22e5c
LuckyG0ldfish <91161929+LuckyG0ldfish@users.noreply.github.com>
2023-07-01 14:26:42 +0200

    Update Schema.ts

--- webapp/formengine/Schema.ts
+++ webapp/formengine/Schema.ts
@@ -169,6 +169,13 @@ export interface ChoiceOption {
 }
 
 /**
+ * Single checkbox
+ */
+export interface CheckboxFormSchemaElement extends DynamicFormSchemaElement<'checkbox'> {
+  option: ChoiceOption;
+}
+
+/**
  * RadioButtons with multiple options
  */
 export interface SingleChoiceFormSchemaElement extends DynamicFormSchemaElement<'single-choice'> {
