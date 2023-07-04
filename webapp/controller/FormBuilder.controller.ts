497392d8223a6d3bed413b78315925f234e8d1ab
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2023-07-04 18:06:07 +0200

    Migrate form engine into own view.

--- webapp/controller/FormBuilder.controller.ts
+++ webapp/controller/FormBuilder.controller.ts
@@ -1,30 +1,8 @@
 import { formBuilderState } from '../formbuilder/State';
 import BaseController from './BaseController';
-import MessageBox from 'sap/m/MessageBox';
 
 export default class FormBuilderController extends BaseController {
   public onInit() {
     this.connectState(formBuilderState);
-  }
-
-  onPreviousPagePress() {
-    formBuilderState.get().goBackward();
-  }
-
-  onNextPagePress() {
-    formBuilderState.get().goForward();
-  }
-
-  onSubmitPress() {
-    if (formBuilderState.get().submit()) {
-      MessageBox.success(
-        'The form would have been submitted successfully. You can see the console output for details about the final form engine state.',
-        { title: 'Form Submitted Successfully' },
-      );
-
-      console.group('📄 Submitted Form state: ');
-      console.info(formBuilderState.get().state);
-      console.groupEnd();
-    }
   }
 }
