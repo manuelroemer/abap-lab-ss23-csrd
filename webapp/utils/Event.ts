fd2fc347270e7d88ca7d519a9e33e57ea09ae27d
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2023-07-14 15:01:47 +0200

    Fix weird issue.

--- webapp/utils/Event.ts
+++ webapp/utils/Event.ts
@@ -1,14 +1,13 @@
 export function entityFromEvent<T = unknown>(e, modelName: string): T | undefined {
-  const source = e.getSource();
-  if (!source) {
-    console.warn('No item could be retrieved from the event source.', e);
-    return undefined;
-  }
-
-  return entityFromSource<T>(source, modelName);
+  return entityFromSource<T>(e.getSource(), modelName);
 }
 
 export function entityFromSource<T = unknown>(source: any, modelName: string): T | undefined {
+  if (!source) {
+    console.warn('No item could be retrieved from the event source.');
+    return undefined;
+  }
+
   const bindingContext = source.getBindingContext(modelName);
   if (!bindingContext) {
     console.warn(`No binding context could be retrieved from the source. modelName: ${modelName}`, source);
