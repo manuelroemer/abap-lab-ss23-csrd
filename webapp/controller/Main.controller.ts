8a3e4fcb3cae5d3c6da6b9583cd8cac4afd4b1d2
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2023-07-17 13:01:05 +0200

    Preparations for clearing the backend.

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -2,6 +2,9 @@ import FilterOperator from 'sap/ui/model/FilterOperator';
 import BaseController from './BaseController';
 import Dialog from 'sap/m/SelectDialog';
 import Filter from 'sap/ui/model/Filter';
+import { showConfirmation } from '../utils/Confirmation';
+import { clearBackend } from '../api/Clear';
+import MessageToast from 'sap/m/MessageToast';
 
 export default class MainController extends BaseController {
   navToFormSchemaManagement() {
@@ -35,5 +38,23 @@ export default class MainController extends BaseController {
       this.router.navTo('Questionnaire', { formSchemaType: 'demo', customerId });
     }
     e.getSource().getBinding('items').filter([]);
+  }
+
+  async onNukeBackend() {
+    if (
+      await showConfirmation({
+        title: 'Clear Backend',
+        text: 'This action will clear ALL data stored in the backend and recreate some default entities that demonstrate how the application can be used.\nAll custom data that was created in the meantime will be completely erased.\nDoing this is perfect if you want to test the application, as it guarantees a clean state to start from. It is also recommended to use this function before doing any demonstration.\n\nAre you sure that you want to continue?',
+      })
+    ) {
+      try {
+        MessageToast.show('Clearing the backend started. Please wait until the next notification.');
+        await clearBackend();
+        MessageToast.show('The backend was successfully cleared. We recommend reloading the current browser tab.');
+      } catch (e) {
+        console.error('Unexpected error while clearing the backend: ', e);
+        MessageToast.show('An error occurred while clearing the backend. Check the browser console for details.');
+      }
+    }
   }
 }
